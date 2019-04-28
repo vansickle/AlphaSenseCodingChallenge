cdc616402a3b8f2550c8d6a4232ed6eabb719c39
Pavel Samokha <pavel@samokha.com>
2019-04-28 18:03:20 +0300

    Add Find subarray with given sum O(n^2)

---
+++ src/main/java/GivenSumChecker.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class GivenSumChecker {
+
+	/**
+	 * Given an unsorted array of nonnegative integers returns true
+	 * if there is a continuous subarray which adds to a given number (target sum).
+	 * @param ints unsorted array of nonnegative integers
+	 * @param target target sum
+	 * @return
+	 */
+	public Boolean check(int[] ints, int target) {
+
+		//store seen sums less than target
+		LinkedHashSet<Integer> seenSums = new LinkedHashSet<>();
+
+		for (int anInt : ints) { //O(n)
+
+			//suppose that target in array fits requirements
+			if(anInt == target) return true;
+
+			//obviously all ints that larger than target won't work
+			if(anInt > target) continue;
+
+			//
+			int addAnIntToTarget = target - anInt;
+
+			if(seenSums.contains(addAnIntToTarget)) //O(1)
+				return true;
+
+			LinkedHashSet<Integer> newSeenSums = new LinkedHashSet<>();
+			newSeenSums.add(anInt);
+			for (Integer seenSum : seenSums) { //O(n)
+				Integer newSeenSum = seenSum + anInt;
+
+				if(!newSeenSums.contains(newSeenSum))
+					newSeenSums.add(newSeenSum);
+			}
+			seenSums = newSeenSums;
+		}
+
+		return false;
+	} //overall complexity = O(n^2)
+}
---
+++ src/test/java/GivenSumCheckerTest.java
@@ -0,0 +1,36 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class GivenSumCheckerTest {
+
+	private GivenSumChecker givenSumChecker;
+
+	@Before
+	public void setUp() throws Exception {
+		givenSumChecker = new GivenSumChecker();
+	}
+
+	@Test
+	public void whenTargetIsSumOfTwo_thenTrue(){
+		assertThat(givenSumChecker.check(new int[]{1,2,3,4}, 7), is(true));
+	}
+
+	@Test
+	public void whenNoTargetSum_thenFalse(){
+		assertThat(givenSumChecker.check(new int[]{1,2,3,4}, 8), is(false));
+	}
+
+	@Test
+	public void whenTargetIsSumOfThree_thenTrue(){
+		assertThat(givenSumChecker.check(new int[]{1,2,3,4}, 6), is(true));
+	}
+
+	@Test
+	public void whenTargetInArray_thenTrue(){
+		assertThat(givenSumChecker.check(new int[]{1,2,3,4}, 4), is(true));
+	}
+}
